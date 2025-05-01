ccc804cb9fac726e00eabf52345a3e75253d06a0
KZLenin <leninmateo4@gmail.com>
2025-05-01 00:02:18 -0500

    first caraceteritics of te page Amazon

---
+++ amazon.js
@@ -0,0 +1,21 @@
+
+
+
+//Strings
+
+let nombreProducto = "CyberPowerPC Gamer Xtreme VR Gaming PC, Intel Core i5-13400F 2.5GHz, GeForce RTX 4060 8GB, 16GB DDR5, 1TB PCIe Gen4 SSD, WiFi Ready y Windows 11 Home (GXiVR8060A24)"
+let precioProducto = 899
+let productoDisponible = true
+let valoracionProducto = 4.5
+let detalleProducto ={
+    marca:"CyberpowerPC",
+    SO: "Windows 11 Home",
+    CPU: "Core i5",
+    tarjetaGrafica: "dedicada",
+    velocidadCPU: "2.5 Ghz",
+    almacenamiento: "1Tb",
+    ranurasMemoria: 4,
+    usoProducto: "Videojuegos",
+    tipoComputadora: "Torre de ordenador",
+    imagen: "https://m.media-amazon.com/images/I/71rPUJ72P5L._AC_SY355_.jpg"
+} 
